3e118d00767574060f6810116f92c1db085a6b05
azad <ayushrawat88101@gmail.com>
2024-11-10 08:00:49 +0530

    stage 0

---
+++ inJAva/maxthree.java
@@ -0,0 +1,78 @@
+// import java.util.Scanner;
+// public class maxthree{
+    
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the value of a");
+//         int a = sc.nextInt();
+//         System.out.println("enter the value of b");
+//         int b = sc.nextInt();
+
+//         System.out.println("enter the value of c");
+        
+//         int c = sc.nextInt();
+
+//        if (a>b&&a>c) {
+//         System.out.println("a is greater");
+        
+//        }
+//        else if (b>a&&b>c) {
+//         System.out.println("b is greater");
+        
+//        }
+//        else {
+//         System.out.println("c is greater");
+        
+//        }
+
+//     }
+// }
+
+
+
+
+import java.util.Scanner;
+
+public class maxthree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    //     // Get the number of elements
+    //     System.out.print("Enter the number of elements: ");
+    //     int n = sc.nextInt();
+
+    //     // Initialize sum
+    //     double sum = 0;
+
+    //     // Loop to get each number and add to total
+    //     for (int i = 1; i <= n; i++) {
+    //         System.out.print("Enter number " + i + ": ");
+    //         double num = scanner.nextDouble();
+    //         sum += num ;
+    //     }
+
+    //     // Output the total sum
+    // //   System.out.println("The total sum is: " + sum);
+    //     scanner.close();
+
+
+
+
+
+
+    // System.out.println("enter the value of n");
+    // int n = sc.nextInt();
+    // System.out.println("odd no.");
+    // for(int i = 1; i<=n;i++){
+    //     if (i%2==1) {
+    //         System.out.println(i);
+            
+    //     }
+    // }
+
+// System.out.println("नमस्ते");
+
+
+
+    }
+}
