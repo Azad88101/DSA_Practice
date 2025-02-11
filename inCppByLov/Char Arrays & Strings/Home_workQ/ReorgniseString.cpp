5ddb881bac81c931838b77b28f4735c8a0d1103e
azad <ayushrawat88101@gmail.com>
2025-02-11 16:42:55 +0530

    Complete implementation of string reorganization method 2
    - Finalized `reorgnise2()` function with frequency-based string reorganization
    - Implemented logic to place most frequent character at even indices
    - Added handling for cases where reorganization is not possible
    - Filled remaining characters at alternating indices
    - Updated comments and code structure for clarity

--- inCppByLov/Char Arrays & Strings/Home_workQ/ReorgniseString.cpp
+++ inCppByLov/Char Arrays & Strings/Home_workQ/ReorgniseString.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <climits>
 using namespace std;
-// metjod 1 i mak thiss  using two pointer approach
+// metjod 1 i mak thiss  using two pointer approach 111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
 string reorgnise1(string s)
 {
 
@@ -42,7 +42,8 @@ string reorgnise1(string s)
 
     return s;
 }
-// method 2 by bubbor
+
+// method 2 by bubbor 22222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
 string reorgnise2(string s)
 {
 
@@ -55,10 +56,42 @@ string reorgnise2(string s)
     char most_frequent;
 
     int frequency = INT_MIN;
-    for (int i = 0; i < s.length(); i++)
+    for (int i = 0; i < 26; i++)
     {
-        /* code */
+        if (hash[i] > frequency)
+        {
+            frequency = hash[i];
+            most_frequent = i + 'a';
+        }
     }
+
+    int in = 0;
+    while (frequency > 0 && in < s.length())
+    {
+        s[in] = most_frequent;
+        in += 2;
+        frequency--;
+    }
+
+    if (frequency != 0)
+    {
+
+        return "";
+    }
+
+    hash[most_frequent - 'a'] = 0;
+    for (int i = 0; i < 26; i++)
+    {
+        while (hash[i] > 0)
+        {
+            in = in >= s.length() ? 1 : in;
+            s[in] = i + 'a';
+            in += 2;
+            hash[i]--;
+        }
+    }
+
+    return s;
 }
 
 int main(int argc, char const *argv[])
