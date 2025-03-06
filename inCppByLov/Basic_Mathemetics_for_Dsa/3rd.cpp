8bcfb80fe862e8382c794a2be68c56262635b68b
azad <ayushrawat88101@gmail.com>
2025-03-06 20:47:43 +0530

    Add mathematical algorithms and pointer exploration
    - Implemented GCD and LCM calculation in `Gcd_Of_TWO_NO.cpp`
    - Added prime number counting using Sieve of Eratosthenes in `Prime.cpp`
    - Created pointer exploration examples in multiple files under Pointers directory
    - Added string manipulation algorithms in Char Arrays & Strings section
    - Updated VSCode settings with additional C++ headers

---
+++ inCppByLov/Basic_Mathemetics_for_Dsa/3rd.cpp
@@ -0,0 +1,31 @@
+// #include <iostream>
+// using namespace std;
+// int FastSquare(int n, int pow)
+// {
+
+//     int ans = 1;
+//     while (pow > 0)
+//     {
+//         if (pow % 2 != 0)
+//         {
+//             ans = ans * n;
+//         }
+//         n = n * n;
+
+//         pow /= 2;
+//     }
+//     return ans;
+// }
+
+// int main(int argc, char const *argv[])
+// {
+//     int n = 5;
+//     int pow =0;
+//     int ans = FastSquare(n, pow);
+//     cout << ans;
+//     return 0;
+// }
+
+
+
+
