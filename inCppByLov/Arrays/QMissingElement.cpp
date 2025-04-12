950e99ba98e59c418990408d7224aeda318eb203
azad <ayushrawat88101@gmail.com>
2025-04-12 15:36:21 +0530

    Add new algorithms for array and string manipulation
    - Introduced `2ndLargest_number.cpp` to find the second largest element in an array
    - Added `common_Element_IN_3sorted_array.cpp` to find common elements in three sorted arrays
    - Implemented `start.cpp` for 2D array operations including row sums and finding the two largest numbers
    - Created `Segmented_sieve.cpp` for prime number generation using the segmented sieve method
    - Added `Longest_substring_without_reapeating_char.cpp` to find the length of the longest substring without repeating characters
    - Included `ATM_MACHINE_PROBLEM.cpp` to simulate money allocation in an ATM scenario
    - Added recursive implementations for factorial and Fibonacci calculations in `factorial.cpp` and `fibonacci.cpp`
    - Updated VSCode settings to include additional C++ headers

--- inCppByLov/Arrays/QMissingElement.cpp
+++ inCppByLov/Arrays/QMissingElement.cpp
@@ -1,29 +1,36 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <unordered_map>
 using namespace std;
+
+int missing(vector<int> arr)
+{
+    unordered_map<int, int> mp;
+
+    int i = 0;
+
+    while (i < arr.size())
+    {
+        mp[arr[i]]++;
+        i++;
+    }
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+
+        if (mp[arr[i]] > 1)
+        {
+        return i;
+        }
+    }
+    return -1;
+}
+
 int main(int argc, char const *argv[])
 {
-    vector<int> arr{1, 3, 5, 3, 4};
+    vector<int> arr{1, 5, 3, 4, 3, 5, 6};
     int i = 1;
-    // while (i < arr.size())
-    // {
-    //     if (arr[i] == i)
-    //     {
-    //         i++;
-    //     }
-    //     else
-    //     {
-    //         swap(arr[i], arr[arr[i]]);
-    //     }
-    // }
-    
-   
-    
-    
-    for (int j = 0; j < arr.size(); j++)
-    {
-        cout << arr[j];
-    }
 
     return 0;
 }
